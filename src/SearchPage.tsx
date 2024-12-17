29e03b15aab3c932021a34dbab30aeb8c35d20c2
aimajohn <aimajoke16@gmail.com>
2024-12-17 15:59:19 -0500

    ad panel on search page

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -7,6 +7,7 @@ import { MovieT, MovieDetailedT } from "@/TYPES_CREATED"
 import { useParams } from "react-router"
 import { Switch } from "@ui/switch"
 import { Label } from "@ui/label"
+import batabitAd from "@/imgs/batabit.png"
 import {
   Select,
   SelectItem,
@@ -72,8 +73,37 @@ export function SearchPage({}: Props) {
     <>
       <Header movieData={null} />
       <div className="flex min-h-svh w-full pt-20">
-        <section className="w-1/4 rounded-lg bg-blue-50 bg-opacity-5 py-8">
-          <div className="mx-auto size-64 bg-black"></div>
+        <section className="my-8 w-1/4 rounded-xl bg-blue-50 bg-opacity-5 py-8 opacity-90">
+          <h1 className="mb-6 text-center font-Montserrat text-xl font-bold text-slate-200">
+            Tablero Proyectos
+          </h1>
+          <div className="mx-6 mb-6 overflow-hidden rounded-lg bg-slate-100 pb-4">
+            <img src={batabitAd} alt="batabit Ad" />
+            <div className="mx-4 mt-4 text-slate-800">
+              <h2 className="font-bold">Proyecto Batabit</h2>
+              <span className="font-Montserrat text-sm text-opacity-20">
+                <b className="font-semibold">Release date: </b>April 2021{" "}
+              </span>
+            </div>
+          </div>
+          <div className="mx-6 mb-6 overflow-hidden rounded-lg bg-slate-100 pb-4">
+            <img src={batabitAd} alt="batabit Ad" />
+            <div className="mx-4 mt-4 text-slate-800">
+              <h2 className="font-bold">Proyecto Batabit</h2>
+              <span className="font-Montserrat text-sm text-opacity-20">
+                <b className="font-semibold">Release date: </b>April 2021{" "}
+              </span>
+            </div>
+          </div>
+          <div className="mx-6 mb-6 overflow-hidden rounded-lg bg-slate-100 pb-4">
+            <img src={batabitAd} alt="batabit Ad" />
+            <div className="mx-4 mt-4 text-slate-800">
+              <h2 className="font-bold">Proyecto Batabit</h2>
+              <span className="font-Montserrat text-sm text-opacity-20">
+                <b className="font-semibold">Release date: </b>April 2021{" "}
+              </span>
+            </div>
+          </div>
         </section>
         <section className="w-full px-8 py-4 text-slate-200">
           <div className="my-4 flex items-center justify-between rounded-lg bg-blue-200 bg-opacity-5 px-8 py-6">
