2157dba7649a6a3772896aadbc5c6c2575e582f2
aimajohn <aimajoke16@gmail.com>
2024-12-18 21:18:16 -0500

    Footer adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@ import MooviesLogo from "@/imgs/logo.png"
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 flex h-16 w-full justify-between bg-yellow-500 px-8 py-2">
-      <div className="flex items-center">
-        <div className="mr-8 flex flex-col items-center text-center">
+    <footer className="absolute bottom-0 flex h-20 w-full flex-col justify-center bg-yellow-500 px-8 py-2 lg:flex-row lg:items-center lg:justify-between">
+      <div className="flex items-center justify-between gap-4">
+        <div className="mr-auto flex flex-col items-center lg:mr-8">
           <img src={Logo} className="w-10" alt="Aimajohn Logo" />
           <span className="text-xs">Aimajohn</span>
         </div>
         <span>Hecho con ❤️ por John</span>
       </div>
-      <div className="my-auto">
+      <div className="my-auto hidden lg:block">
         <img className="h-10" src={MooviesLogo} alt="Logo The Moovies" />
       </div>
     </footer>
